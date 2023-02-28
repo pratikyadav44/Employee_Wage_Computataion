c718fd380706da08a1e5dfa7723f0f796ed08977
pratikyadav44 <pratik.yadav2@gmail.com>
2023-02-28 19:15:30 +0530

    [Pratik]

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Employee_wage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeAttendance.AttendanceCheck();
+            DailyWage.Daily_Wage_Check();
+        }
+    }
+}
